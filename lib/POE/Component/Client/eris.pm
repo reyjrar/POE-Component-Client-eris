93b9e069bf23999575015b9c9c7337791194872c
Brad Lhotsky <brad.lhotsky@booking.com>
2012-11-08 11:54:06 +0100

    Retabbed to spaces

--- lib/POE/Component/Client/eris.pm
+++ lib/POE/Component/Client/eris.pm
@@ -8,7 +8,7 @@ use Carp;
 use Parse::Syslog::Line;
 
 use POE qw(
-	Component::Client::TCP
+    Component::Client::TCP
 );
 
 our $VERSION = '1.2';
@@ -20,15 +20,15 @@ POE session for integration with the eris event correlation engine.
     use POE::Component::Client::eris;
 
     my $eris_sess_id = POE::Component::Client::eris->spawn(
-			RemoteAddress		=> 'localhost', 	#default
-			RemotePort			=> '9514',		 	#default
-			Alias				=> 'eris_client',	#default
-			Subscribe			=> [qw(snort dhcpd)],				# REQUIRED (and/or Match)
-			Match				=> [qw(devbox1 myusername error)],	# REQUIRED (and/or Subscribe)
-			MessageHandler		=> sub { ... },		 # REQUIRED
-	);
+            RemoteAddress       => 'localhost',     #default
+            RemotePort          => '9514',          #default
+            Alias               => 'eris_client',   #default
+            Subscribe           => [qw(snort dhcpd)],               # REQUIRED (and/or Match)
+            Match               => [qw(devbox1 myusername error)],  # REQUIRED (and/or Subscribe)
+            MessageHandler      => sub { ... },      # REQUIRED
+    );
     ...
-	POE::Kernel->run();
+    POE::Kernel->run();
 
 For use with a server running the POE::Component::Server::eris output.
 
@@ -43,99 +43,99 @@ POE::Component::Client::eris does not export any symbols.
 Creates the POE::Session for the eris correlator.
 
 Parameters:
-	RemoteAddress		=> 'localhost', 	#default
-	RemotePort			=> '9514',		 	#default
-	Alias				=> 'eris_client',	#default
-	Subscribe			=> [qw(snort dhcpd)],				# REQUIRED (and/or Match)
-	Match				=> [qw(devbox1 myusername error)],	# REQUIRED (and/or Subscribe)
-	MessageHandler		=> sub { ... },		 # REQUIRED
+    RemoteAddress       => 'localhost',     #default
+    RemotePort          => '9514',          #default
+    Alias               => 'eris_client',   #default
+    Subscribe           => [qw(snort dhcpd)],               # REQUIRED (and/or Match)
+    Match               => [qw(devbox1 myusername error)],  # REQUIRED (and/or Subscribe)
+    MessageHandler      => sub { ... },      # REQUIRED
     ReturnType          => 'hash',           # default, or 'string'
 
 =cut
 
 sub spawn {
-	my $type = shift;
+    my $type = shift;
 
-	#
-	# Param Setup
-	my %args = (
-		RemoteAddress	=> 'localhost',
-		RemotePort		=> 9514,
-		Alias			=> 'eris_client',
+    #
+    # Param Setup
+    my %args = (
+        RemoteAddress   => 'localhost',
+        RemotePort      => 9514,
+        Alias           => 'eris_client',
         ReturnType      => 'hash',
-		Subscribe		=> undef,
-		Match			=> undef,
-		MessageHandler	=> undef,
-		@_
-	);
+        Subscribe       => undef,
+        Match           => undef,
+        MessageHandler  => undef,
+        @_
+    );
 
-	#
-	# Build the client connection
-	my $tcp_sessid = POE::Component::Client::TCP->new(
-		Alias			=> $args{Alias},
-		RemoteAddress	=> $args{RemoteAddress},
-		RemotePort		=> $args{RemotePort},
-		Filter			=> 'POE::Filter::Line',
-		Connected		=> sub {
-			my ($kernel,$heap) = @_[KERNEL,HEAP];
-			$heap->{readyState} = 0;
-			$heap->{connected} = 0;
-			$kernel->delay( 'do_setup_pipe' => 1 );
-		},
-		ConnectError	=> sub {
-			my ($kernel,$syscall,$errid,$errstr) = @_[KERNEL,ARG0,ARG1,ARG2];
-			carp "Connection Error ($errid) at $syscall: $errstr\n";
-			$kernel->delay('reconnect' => 10);
-		},
-		Disconnected	=> sub {
-			my ($kernel,$heap) = @_[KERNEL,HEAP];
-			$kernel->delay('reconnect' => 10);
-		},
-		ServerError		=> sub  {
-			my ($kernel,$syscall,$errid,$errstr) = @_[KERNEL,ARG0,ARG1,ARG2];
-			carp "Server Error ($errid) at $syscall: $errstr\n";
-			$kernel->delay('reconnect' => 5);
-		},
-		#
-		# Handle messages from the server.
-		#  Set readyState = 1 if applicable
-		#  Call the inline states:
-		#   handle_message (successful)
-		#   handle_unknown (out of order input)
-		ServerInput		=> sub {
-			my ($kernel,$heap,$instr) = @_[KERNEL,HEAP,ARG0];
-			chomp $instr;
-			if( $heap->{readyState} == 1 ) {
-				$kernel->yield('handle_message' => $instr);
-			}
-			elsif( $heap->{connected} == 1 ) {
-				if( $instr =~ /^Subscribed to \:/ ) {
-					$heap->{readyState} = 1;
-				}
-				elsif( $instr =~ /^Receiving / )  {
-					$heap->{readyState} = 1;
-				}
-				elsif( $instr =~ /^Full feed enabled/ )  {
-					$heap->{readyState} = 1;
-				}
-				else {
-					$kernel->yield( 'handle_unknown' => $instr );
-				}
-			}
-			elsif( $instr =~ /^EHLO Streamer/ ) {
-				$heap->{connected} = 1;
-			}
-			else {
-				$kernel->yield( 'handle_unknown' => $instr );
-			}
-		},
-		#
-		# Inline States
-		InlineStates => {
-			do_setup_pipe	=> sub {
-				my ($kernel,$heap) = @_[KERNEL,HEAP];
+    #
+    # Build the client connection
+    my $tcp_sessid = POE::Component::Client::TCP->new(
+        Alias           => $args{Alias},
+        RemoteAddress   => $args{RemoteAddress},
+        RemotePort      => $args{RemotePort},
+        Filter          => 'POE::Filter::Line',
+        Connected       => sub {
+            my ($kernel,$heap) = @_[KERNEL,HEAP];
+            $heap->{readyState} = 0;
+            $heap->{connected} = 0;
+            $kernel->delay( 'do_setup_pipe' => 1 );
+        },
+        ConnectError    => sub {
+            my ($kernel,$syscall,$errid,$errstr) = @_[KERNEL,ARG0,ARG1,ARG2];
+            carp "Connection Error ($errid) at $syscall: $errstr\n";
+            $kernel->delay('reconnect' => 10);
+        },
+        Disconnected    => sub {
+            my ($kernel,$heap) = @_[KERNEL,HEAP];
+            $kernel->delay('reconnect' => 10);
+        },
+        ServerError     => sub  {
+            my ($kernel,$syscall,$errid,$errstr) = @_[KERNEL,ARG0,ARG1,ARG2];
+            carp "Server Error ($errid) at $syscall: $errstr\n";
+            $kernel->delay('reconnect' => 5);
+        },
+        #
+        # Handle messages from the server.
+        #  Set readyState = 1 if applicable
+        #  Call the inline states:
+        #   handle_message (successful)
+        #   handle_unknown (out of order input)
+        ServerInput     => sub {
+            my ($kernel,$heap,$instr) = @_[KERNEL,HEAP,ARG0];
+            chomp $instr;
+            if( $heap->{readyState} == 1 ) {
+                $kernel->yield('handle_message' => $instr);
+            }
+            elsif( $heap->{connected} == 1 ) {
+                if( $instr =~ /^Subscribed to \:/ ) {
+                    $heap->{readyState} = 1;
+                }
+                elsif( $instr =~ /^Receiving / )  {
+                    $heap->{readyState} = 1;
+                }
+                elsif( $instr =~ /^Full feed enabled/ )  {
+                    $heap->{readyState} = 1;
+                }
+                else {
+                    $kernel->yield( 'handle_unknown' => $instr );
+                }
+            }
+            elsif( $instr =~ /^EHLO Streamer/ ) {
+                $heap->{connected} = 1;
+            }
+            else {
+                $kernel->yield( 'handle_unknown' => $instr );
+            }
+        },
+        #
+        # Inline States
+        InlineStates => {
+            do_setup_pipe   => sub {
+                my ($kernel,$heap) = @_[KERNEL,HEAP];
 
-				# Parse for Subscriptions or Matches
+                # Parse for Subscriptions or Matches
                 my %data = ();
                 foreach my $target (qw(Subscribe Match)) {
                     if( exists $args{$target} && defined $args{$target} ) {
@@ -146,32 +146,32 @@ sub spawn {
                     }
                 }
 
-				# Check to make sure we're doing something
-				croak "Must specify a subscription or a match parameter!\n" unless keys %data;
+                # Check to make sure we're doing something
+                croak "Must specify a subscription or a match parameter!\n" unless keys %data;
 
-				# Send the Subscription
+                # Send the Subscription
                 foreach my $target (sort { $a cmp $b } keys %data) {
                     my $subname = "do_" . lc $target;
-				    $kernel->yield( $subname => $data{$target} );
+                    $kernel->yield( $subname => $data{$target} );
                 }
-			},
-			do_subscribe	=> sub {
-				my ($kernel,$heap,$subs) = @_[KERNEL,HEAP,ARG0];
+            },
+            do_subscribe    => sub {
+                my ($kernel,$heap,$subs) = @_[KERNEL,HEAP,ARG0];
 
-				if( grep /^fullfeed$/, @{ $subs } ) {
-					$heap->{server}->put('fullfeed');
-				}
-				else {
-					$heap->{server}->put('sub ' . join(', ', @{ $subs }) );
-				}
-			},
-			do_match	=> sub {
-				my ($kernel,$heap,$matches) = @_[KERNEL,HEAP,ARG0];
+                if( grep /^fullfeed$/, @{ $subs } ) {
+                    $heap->{server}->put('fullfeed');
+                }
+                else {
+                    $heap->{server}->put('sub ' . join(', ', @{ $subs }) );
+                }
+            },
+            do_match    => sub {
+                my ($kernel,$heap,$matches) = @_[KERNEL,HEAP,ARG0];
 
-				$heap->{server}->put('match ' . join(', ', @{ $matches }) );
-			},
-			handle_message	=> sub {
-				my ($kernel,$heap,$instr) = @_[KERNEL,HEAP,ARG0];
+                $heap->{server}->put('match ' . join(', ', @{ $matches }) );
+            },
+            handle_message  => sub {
+                my ($kernel,$heap,$instr) = @_[KERNEL,HEAP,ARG0];
 
                 my $msg = undef;
                 if( $args{ReturnType} eq 'string' ) {
@@ -187,25 +187,25 @@ sub spawn {
                     }
                 }
 
-				if( ref $args{MessageHandler} ne 'CODE' ) {
-					croak "You need to specify a subroutine reference to the 'MessageHandler' parameter.\n";
-				}
-				# Try the Message Handler, eventually we can do statistics here.
-				eval {
-					$args{MessageHandler}->( $msg );
-				};
-			},
-			handle_unknown	=> sub {
-				my ($kernel,$heap,$msg) = @_[KERNEL,HEAP,ARG0];
+                if( ref $args{MessageHandler} ne 'CODE' ) {
+                    croak "You need to specify a subroutine reference to the 'MessageHandler' parameter.\n";
+                }
+                # Try the Message Handler, eventually we can do statistics here.
+                eval {
+                    $args{MessageHandler}->( $msg );
+                };
+            },
+            handle_unknown  => sub {
+                my ($kernel,$heap,$msg) = @_[KERNEL,HEAP,ARG0];
 
-				carp "UNKNOWN INPUT: $msg\n";
-			},
-		},
-	);
+                carp "UNKNOWN INPUT: $msg\n";
+            },
+        },
+    );
 
-	#
-	# Return the TCP Session ID
-	return $tcp_sessid;
+    #
+    # Return the TCP Session ID
+    return $tcp_sessid;
 }
 
 =head1 AUTHOR
